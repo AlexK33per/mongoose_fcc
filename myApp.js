cdf88cebb541601a7cab367d6e62d321b3f3aac6
k33per <aalonsopersonal@gmail.com>
2022-03-19 03:07:19 +0100

    moved model to myApp.js

--- myApp.js
+++ myApp.js
@@ -5,6 +5,16 @@ let Person;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+    name: { type: String, required: true },
+    age: Number,
+    favoriteFoods: [String]
+});
+
+mongoose.model('Person', personSchema);
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
